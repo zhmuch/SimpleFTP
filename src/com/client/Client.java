fb4e483464acdcc03d68b451c5d7cdac3b3468b0
zhmuch <muchenzhang@hotmail.com>
2016-04-01 23:02:55 -0400

    first

---
+++ src/com/client/Client.java
@@ -0,0 +1,10 @@
+package com.client;
+
+
+/**
+ * Created by Muchen on 4/1/16.
+ */
+
+
+public class Client {
+}
---
+++ src/com/server/Server.java
@@ -0,0 +1,8 @@
+package com.server;
+
+/**
+ * Created by Muchen on 4/1/16.
+ */
+public class Server {
+
+}
