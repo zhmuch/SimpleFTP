3a2bb3670d2b504c7b0f81faa22c6678744f2da0
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-04-20 04:17:37 -0400

    Create Retransmitter.java

---
+++ SelectiveRepeat/src/com/client/Retransmitter.java
@@ -0,0 +1,37 @@
+package com.client;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by Muchen on 4/20/16.
+ */
+public class Retransmitter extends TimerTask {
+
+    private int sequ;
+
+    public Retransmitter(int i) {
+        this.sequ = i;
+    }
+
+    @Override
+    public void run(){
+
+//        System.out.println("Timeout! Retransmit! Sequence Number: " + sequ);
+
+        Simple_ftp_client.cancel(sequ);
+
+        try {
+
+//            System.out.println("Without Exception");
+            Simple_ftp_client.rdt_send(Simple_ftp_client.fileBytes.get(sequ), sequ);
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        Simple_ftp_client.setTimer(sequ);
+
+    }
+}
